667b8c8680512f70beb1fdbf7313cc78d61471ed
Bruno Azevedo <93809602+brunoacev@users.noreply.github.com>
2022-11-03 15:20:04 -0300

    Feat: Teste de envio para repo

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import "reflect-metadata";
-import "express-async-errors";
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerDoc from "./swagger.json";
 
 const app = express();
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,13 @@
 import app from "./app";
 import AppDataSource from "./data-source";
 
-(async () => {
 
-    await AppDataSource.initialize()
-    .catch((err) => {
-        console.error("Error during Data Source initialization", err)
-    })
-    
-    app.listen(3000, () => {
-        console.log("Servidor executando")
-    })    
+(async () => {
+  await AppDataSource.initialize().catch((err) => {
+    console.error("Error during Data Source initialization", err);
+  });
+
+  app.listen(3000, () => {
+    console.log("Servidor executando");
+  });
 })();
