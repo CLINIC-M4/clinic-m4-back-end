0e0ba17a5237a1baeb551c1571528af12b334390
Mrdiskman <pedrosandes2010@gmail.com>
2022-11-04 12:45:12 -0300

    feat: updateDoctor/deleteDoctor

--- src/routes/doctor/doctor.routes.ts
+++ src/routes/doctor/doctor.routes.ts
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import deleteDoctorController from "../../controller/doctor/deleteDoctor.controller";
+import doctorUpdateController from "../../controller/doctor/doctorUpdate.Controller";
 import listDoctorController from "../../controller/doctor/listDoctor.Controller";
 import doctorLoginController from "../../controller/doctor/loginDoctor.controller";
 import registerDoctorController from "../../controller/doctor/registerDoctor.Controller";
@@ -11,5 +13,7 @@ const router = Router();
 router.post("", verifyEmail, registerDoctorController);
 router.post("/login", doctorLoginController)
 router.get("", ensureAuthMiddleware, verifyIsAdmMiddleware, listDoctorController);
+router.patch("/:id", ensureAuthMiddleware, verifyIsAdmMiddleware, doctorUpdateController)
+router.delete("/:id", ensureAuthMiddleware, verifyIsAdmMiddleware, deleteDoctorController)
 
 export default router;
