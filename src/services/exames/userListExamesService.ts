52041b653f1d0d4ff49a0a6ee9901978b7dd909f
Lucas Maciel <lucasm.contato@hotmail.com>
2022-11-04 17:25:05 -0300

    feat:Correção usuário não pode ver outros exames a não ser o dele

--- src/services/exames/userListExamesService.ts
+++ src/services/exames/userListExamesService.ts
@@ -3,7 +3,15 @@ import { appError } from "../../errors/appError";
 import { ExamesUserDoctor } from "../../entities/examesUserDoctor.entity";
 import { IExames } from "../../interfaces/exames/exames";
 
-const userListExamesService = async (id: string): Promise<IExames[]> => {
+const userListExamesService = async (
+  id: string,
+  idUser: String
+): Promise<IExames[]> => {
+  const sameId = id == idUser;
+
+  if (!sameId) {
+    throw new appError(400, "Exam not found.");
+  }
   const examRepository = AppDataSource.getRepository(ExamesUserDoctor);
   const exames = await examRepository.findBy({ id });
   if (exames == null || exames == undefined || !exames) {
