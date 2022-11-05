e8f0dd969c2e8c5e426bc41a780b6680d2637f33
Bruno Azevedo <93809602+brunoacev@users.noreply.github.com>
2022-11-04 21:01:19 -0300

    Feat: Adição da lib SwaggerUi, Criação de escopo JSON da documentação e Implementação da rota

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,11 @@
 import usersRouter from "./users/user.routes";
-import doctorRouter from "./doctor/doctor.routes"
+import doctorRouter from "./doctor/doctor.routes";
 import { Express } from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocs from "../swagger.json";
 
 export const initializerRouter = (app: Express) => {
-    app.use("/users", usersRouter);
-    app.use("/doctor", doctorRouter);
-}
-
-
+  app.use("/users", usersRouter);
+  app.use("/doctor", doctorRouter);
+  app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+};
